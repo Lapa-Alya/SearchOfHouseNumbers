e6426de4c520b3989998ca1306071665a8c7f0e2
Lapa-Alya <i-sunny@mail.ru>
2015-12-13 01:10:57 +0300

    Add new functions
    New functions: public double DiffR2(Contour c), public double Norma,
    public unsafe Complex Dot(Contour c, int shift)

--- CA/Contour.cs
+++ CA/Contour.cs
@@ -73,5 +73,70 @@ namespace ContourAnalysisNS
             result.array = (Complex[])array.Clone();
             return result;
         }
+        /// <summary>
+        /// Returns R^2 of difference of norms
+        /// </summary>
+        /// <param name="c"></param>
+        /// <returns></returns>
+        public double DiffR2(Contour c)
+        {
+            double max1 = 0;
+            double max2 = 0;
+            double sum = 0;
+            for (int i = 0; i < Count; i++)
+            {
+                double v1 = array[i].Norma;
+                double v2 = c.array[i].Norma;
+                if (v1 > max1) max1 = v1;
+                if (v2 > max2) max2 = v2;
+                double v = v1 - v2;
+                sum += v * v;
+            }
+            double max = Math.Max(max1, max2);
+            return 1 - sum / Count / max / max;
+        }
+
+        public double Norma
+        {
+            get
+            {
+                double result = 0;
+                foreach (var c in array)
+                    result += c.NormaSquare;
+                return Math.Sqrt(result);
+            }
+        }
+
+        /// <summary>
+        /// Scalar product
+        /// </summary>
+        public unsafe Complex Dot(Contour c, int shift)
+        {
+            var count = Count;
+            double sumA = 0;
+            double sumB = 0;
+            fixed (Complex* ptr1 = &array[0])
+            fixed (Complex* ptr2 = &c.array[shift])
+            fixed (Complex* ptr22 = &c.array[0])
+            fixed (Complex* ptr3 = &c.array[c.Count - 1])
+            {
+                Complex* p1 = ptr1;
+                Complex* p2 = ptr2;
+                for (int i = 0; i < count; i++)
+                {
+                    Complex x1 = *p1;
+                    Complex x2 = *p2;
+                    sumA += x1.a * x2.a + x1.b * x2.b;
+                    sumB += x1.b * x2.a - x1.a * x2.b;
+
+                    p1++;
+                    if (p2 == ptr3)
+                        p2 = ptr22;
+                    else
+                        p2++;
+                }
+            }
+            return new Complex(sumA, sumB);
+        }
     }
 }
