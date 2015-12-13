fa83f62e2319d58570b94571ea2d4890636a0d33
emmure61 <pashkin1995@OUTLOOK.com>
2015-12-13 23:31:30 +0300

    Template

--- CA/Template.cs
+++ CA/Template.cs
@@ -67,4 +67,56 @@ namespace ContourAnalysisNS
             autoCorrDescriptor4 = (int)(100 * sum4 / count);
         }
     }
+    public void Draw(Graphics gr, Rectangle rect)
+        {
+            gr.DrawRectangle(Pens.SteelBlue, rect);
+            rect = new Rectangle(rect.Left, rect.Top, rect.Width - 24, rect.Height);
+
+            var contour = this.contour.Clone();
+            var autoCorr = this.autoCorr.Clone();
+            //contour.Normalize();
+            autoCorr.Normalize();
+
+            //draw contour
+            Rectangle r = new Rectangle(rect.X, rect.Y, rect.Width / 2, rect.Height);
+            r.Inflate(-20, -20);
+            var points = contour.GetPoints(startPoint);
+            Rectangle boundRect = Rectangle.Round(contour.GetBoundsRect());
+
+            double w = boundRect.Width;
+            double h = boundRect.Height;
+            float k = (float)Math.Min(r.Width/w, r.Height/h);
+            int dx = startPoint.X - contour.SourceBoundingRect.Left;
+            int dy = startPoint.Y - contour.SourceBoundingRect.Top;
+            int ddx = -(int)(boundRect.Left * k);
+            int ddy = (int)(boundRect.Bottom * k);
+            for (int i = 0; i < points.Length; i++)
+                points[i] = new Point(r.Left + ddx + (int)((points[i].X - contour.SourceBoundingRect.Left - dx) * k), r.Top + ddy + (int)((points[i].Y - contour.SourceBoundingRect.Top - dy) * k));
+            //
+            gr.DrawPolygon(Pens.Red, points);
+            //draw ACF
+            r = new Rectangle(rect.Width / 2 + rect.X, rect.Y, rect.Width / 2, rect.Height);
+            r.Inflate(-20, -20);
+
+            List<Point> angles = new List<Point>();
+            for (int i = 0; i < autoCorr.Count; i++)
+            {
+                int x = r.X + 5 + i * 3;
+                int v = (int)(autoCorr[i % autoCorr.Count].Norma * r.Height);
+                gr.FillRectangle(Brushes.Blue, x, r.Bottom - v, 3, v);
+                angles.Add(new Point(x, r.Bottom - (int)(r.Height * (0.5d + autoCorr[i%autoCorr.Count].Angle / 2 / Math.PI))));
+            }
+
+            try
+            {
+                gr.DrawLines(Pens.Red, angles.ToArray());
+            }
+            catch(OverflowException)
+            { ;}
+
+            
+
+            
+            }
+        }
 }
