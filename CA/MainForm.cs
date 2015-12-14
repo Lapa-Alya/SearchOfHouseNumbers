0792640dc76120bf6b36d7fd933566e80f80816e
Lapa-Alya <i-sunny@mail.ru>
2015-12-15 02:00:16 +0300

    Main form
    Create main form

---
+++ CA/MainForm.cs
@@ -0,0 +1,223 @@
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using ContourAnalysisNS;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+namespace ContourAnalysisDemo
+{
+    public partial class MainForm : Form
+    {
+        Image<Bgr, Byte> frame;
+        ImageProcessor processor;
+        Dictionary<string, Image> AugmentedRealityImages = new Dictionary<string, Image>();
+
+        string templateFile;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            //create image preocessor
+            processor = new ImageProcessor();
+            //load default templates
+            templateFile = AppDomain.CurrentDomain.BaseDirectory + "\\Tahoma.bin";
+            LoadTemplates(templateFile);
+            //apply settings
+            ApplySettings();
+            //
+            Application.Idle += new EventHandler(Application_Idle);
+        }
+
+        private void LoadTemplates(string fileName)
+        {
+            try
+            {
+                using (FileStream fs = new FileStream(fileName, FileMode.Open))
+                    processor.templates = (Templates)new BinaryFormatter().Deserialize(fs);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void SaveTemplates(string fileName)
+        {
+            try
+            {
+                using (FileStream fs = new FileStream(fileName, FileMode.Create))
+                    new BinaryFormatter().Serialize(fs, processor.templates);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        void Application_Idle(object sender, EventArgs e)
+        {
+            ProcessFrame();
+        }
+
+        private void ProcessFrame()
+        {
+            try
+            {
+                //
+                processor.ProcessImage(frame);
+                //
+                if (cbShowBinarized.Checked)
+                    ibMain.Image = processor.binarizedFrame;
+                else
+                    ibMain.Image = frame;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+
+        private void ibMain_Paint(object sender, PaintEventArgs e)
+        {
+            if (frame == null) return;
+
+            Font font = new Font(Font.FontFamily, 24);//16
+
+            Brush bgBrush = new SolidBrush(Color.FromArgb(255, 0, 0, 0));
+            Brush foreBrush = new SolidBrush(Color.FromArgb(255, 255, 255, 255));
+            Pen borderPen = new Pen(Color.FromArgb(150, 0, 255, 0));
+            //
+            if (cbShowContours.Checked)
+                foreach (var contour in processor.contours)
+                    if (contour.Total > 1)
+                        e.Graphics.DrawLines(Pens.Red, contour.ToArray());
+            //
+            lock (processor.foundTemplates)
+            foreach (FoundTemplateDesc found in processor.foundTemplates)
+                {
+                    if (found.template.name.EndsWith(".png") || found.template.name.EndsWith(".jpg"))
+                    {
+                        MessageBox.Show("show");
+                        DrawAugmentedReality(found, e.Graphics);
+                        continue;
+                    }
+
+                    Rectangle foundRect = found.sample.contour.SourceBoundingRect;
+                    Point p1 = new Point((foundRect.Left + foundRect.Right) / 2, foundRect.Top);
+                    string text = found.template.name;
+                    e.Graphics.DrawRectangle(borderPen, foundRect);
+                    e.Graphics.DrawString(text, font, bgBrush, new PointF(p1.X + 1 - font.Height / 3, p1.Y + 1 - font.Height));
+                    e.Graphics.DrawString(text, font, foreBrush, new PointF(p1.X - font.Height / 3, p1.Y - font.Height));
+                }
+        }
+
+        private void DrawAugmentedReality(FoundTemplateDesc found, Graphics gr)
+        {
+           
+        }
+
+        private void cbAutoContrast_CheckedChanged(object sender, EventArgs e)
+        {
+            ApplySettings();
+        }
+
+        private void ApplySettings()
+        {
+            try
+            {
+                processor.equalizeHist = cbAutoContrast.Checked;
+                processor.blur = cbBlur.Checked;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btLoadImage_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btCreateTemplate_Click(object sender, EventArgs e)
+        {
+            if (frame != null)
+                new ShowContoursForm(processor.templates, processor.samples, frame).ShowDialog();
+        }
+
+        private void btNewTemplates_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Do you want to create new template database?", "Create new template database", MessageBoxButtons.OKCancel) == System.Windows.Forms.DialogResult.OK)
+                processor.templates.Clear();
+        }
+
+        private void btOpenTemplates_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Templates(*.bin)|*.bin";
+            if (ofd.ShowDialog(this) == System.Windows.Forms.DialogResult.OK)
+            {
+                templateFile = ofd.FileName;
+                LoadTemplates(templateFile);
+            }
+        }
+
+        private void btSaveTemplates_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "Templates(*.bin)|*.bin";
+            if (sfd.ShowDialog(this) == System.Windows.Forms.DialogResult.OK)
+            {
+                templateFile = sfd.FileName;
+                SaveTemplates(templateFile);
+            }
+        }
+
+        private void btTemplateEditor_Click(object sender, EventArgs e)
+        {
+            new TemplateEditor(processor.templates).Show();
+        }
+
+        private void btAutoGenerate_Click(object sender, EventArgs e)
+        {
+            new AutoGenerateForm(processor).ShowDialog();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox2_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cbShowContours_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox2_Enter_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ibMain_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+    }
+}
